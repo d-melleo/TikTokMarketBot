8e329767d24d065685605bb4b7388613f1e33aba
Melleo <148089993+d-melleo@users.noreply.github.com>
2024-02-12 12:15:40 +0200

    Update connect.py

--- db/connect.py
+++ db/connect.py
@@ -5,16 +5,12 @@ from config.environment_vars import DB_COLLECTION_NAME, DB_CONNECTION_STRING, DB
 
 
 class DBConnect:
-    """A class representing a MongoDB connection using AsyncIOMotorClient.
+    """MongoDB connection using AsyncIOMotorClient.
 
     Attributes:
         client (AsyncIOMotorClient): The MongoDB client connected to the specified database.
         db (AsyncIOMotorDatabase): The MongoDB database instance accessed through the client.
         collection (AsyncIOMotorCollection): The MongoDB collection within the specified database.
-
-    Note:
-        Ensure that the class is used within an asynchronous context, as it utilizes
-        AsyncIOMotorClient for asynchronous MongoDB operations.
     """
     # client = AsyncIOMotorClient(connection_string, server_api=ServerApi("1", strict=True))
     client = AsyncIOMotorClient(DB_CONNECTION_STRING)
